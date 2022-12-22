a23b36aa6fc3e3103ef9fb0f500b18d657b7df81
stvteclas <stvpablo@gmail.com>
2022-12-22 15:22:03 -0300

    first commit

---
+++ src/main/java/com/idforideas/wikideas/WikideasApplication.java
@@ -0,0 +1,13 @@
+package com.idforideas.wikideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikideasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikideasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idforideas/wikideas/WikideasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idforideas.wikideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikideasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
