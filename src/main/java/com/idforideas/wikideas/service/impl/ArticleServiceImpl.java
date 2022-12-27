64a71d22e92beed946befefa36508b4df6248169
stvteclas <stvpablo@gmail.com>
2022-12-26 21:13:30 -0300

    create Article Service , Theme Service and this implementations

---
+++ src/main/java/com/idforideas/wikideas/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,10 @@
+package com.idforideas.wikideas.service.impl;
+
+import com.idforideas.wikideas.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleServiceImpl implements ArticleService {
+}
