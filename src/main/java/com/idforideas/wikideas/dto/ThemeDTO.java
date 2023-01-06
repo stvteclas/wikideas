42d6f8df0eedd6b6b318c0731fe3b373b2bf7b40
stvteclas <stvpablo@gmail.com>
2023-01-06 16:54:36 -0300

    validator fix , config new jar

--- src/main/java/com/idforideas/wikideas/dto/ThemeDTO.java
+++ src/main/java/com/idforideas/wikideas/dto/ThemeDTO.java
@@ -1,7 +1,11 @@
 package com.idforideas.wikideas.dto;
 
+import com.idforideas.wikideas.dto.validator.IValidatorArticle;
+import com.idforideas.wikideas.dto.validator.IValidatorTheme;
 import com.idforideas.wikideas.model.ThemeEntity;
 import com.idforideas.wikideas.model.ThemeEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @ToString
@@ -10,10 +14,11 @@ import lombok.*;
 @Getter
 @Setter
 public class ThemeDTO {
-
+    @NotBlank(groups= {IValidatorTheme.class})
     private Long idTheme;
+    @NotBlank(groups= {IValidatorTheme.class})
     private ThemeEnum theme;
-
+    @NotBlank(groups= {IValidatorTheme.class})
     private String description;
 
     public ThemeDTO(ThemeEntity theme){
