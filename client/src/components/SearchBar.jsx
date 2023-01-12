70fae0a8b0668a8cb2aa5ca116699772489f50d9
jmbvdev <jmvb.dev@gmail.com>
2023-01-12 16:20:50 -0400

    autocomplete

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import s from "../styles/search.module.css"
 import {TbSearch} from "react-icons/tb"
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+    const articles = useSelector((state) => state.articlesReducers.articles);
+console.log(searchTerm)
+    const handleChange = (event) => {
+      setSearchTerm(event.target.value);
+      const filteredArticles = articles.filter((article) =>
+        article.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSuggestions(filteredArticles);
+    };
     return (
-       <form action="" className={s.container}>
-        <input type="text" placeholder='Search...'/>
+       <div action="" className={s.container}>
+        <div className={s.searchbar}>
+
+        <input type="text" placeholder='Search...' value={searchTerm} onChange={handleChange}/>
         <button>
             <TbSearch/>
         </button>
+        </div>
 
-       </form>
+       {
+        searchTerm.length>0&&
+
+       <ul className={s.ul_suggestions}>
+        {suggestions.map((suggestion) => (
+            <div key={suggestion.id} className={s.suggestion}>
+                <img src={suggestion.image} alt="" />
+              <li>{suggestion.title}</li>
+            </div>
+          
+        ))}
+      </ul>
+       }
+       </div>
+     
     );
 };
 
