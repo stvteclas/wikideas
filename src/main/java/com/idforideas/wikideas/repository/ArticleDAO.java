78e88847434cd7eacf5de608a33d64037f8d8d5d
stvteclas <stvpablo@gmail.com>
2023-01-16 15:36:30 -0300

    Article Test

--- src/main/java/com/idforideas/wikideas/repository/ArticleDAO.java
+++ src/main/java/com/idforideas/wikideas/repository/ArticleDAO.java
@@ -54,7 +54,7 @@ public class ArticleDAO {
 
     public ArticleEntity updateArticle(Long id, ArticleDTO article){
         Optional<ArticleEntity> articleUpdate = articleRepository.findById(id);
-        if (!articleUpdate.isPresent()){
+            if (!articleUpdate.isPresent()){
             throw new WikiException("Article does not exist");
         }
         ThemeEntity themeEntity = themeRepository.findByTheme(article.getTheme());
@@ -76,7 +76,7 @@ public class ArticleDAO {
         if (!articleExist.isPresent()){
             throw new WikiException(MessageErrorEnum.INVALID_TITLE.getMessage());
         }
-       return articleRepository.findArticleByTitle(articleExist.get());
+        return articleRepository.findArticleByTitle(articleExist.get());
     }
 
     public List<ArticleDTO> getAll() {
