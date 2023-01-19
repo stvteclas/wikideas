970ac927366e0c234d32e378cd76299ac41cf5e6
stvteclas <stvpablo@gmail.com>
2023-01-19 09:01:16 -0300

    deploy config

--- src/main/java/com/idforideas/wikideas/controller/ThemeController.java
+++ src/main/java/com/idforideas/wikideas/controller/ThemeController.java
@@ -9,17 +9,20 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-//@CrossOrigin(origins = "*", methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.PATCH, RequestMethod.POST, RequestMethod.OPTIONS})
+@CrossOrigin(origins = "*", methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.PATCH, RequestMethod.POST, RequestMethod.OPTIONS})
 @RequestMapping(value = "/themes")
 public class ThemeController {
 
+
     private final ThemeService themeService;
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping()
     public List<ThemeDTO> showAllThemes (){
         return themeService.showAllThemes();
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/{id}")
     public List<ThemeDTO> getThemeById(@PathVariable Long id){
         return themeService.getThemeById(id);
