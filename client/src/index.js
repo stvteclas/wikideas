756c4998ee5c53b27599d2bf7583a0f36545dda1
jmbvdev <jmvb.dev@gmail.com>
2023-01-19 13:03:10 -0400

    headers axios

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-
 import {Provider} from "react-redux"
 import store from './redux/store/index.js';
+import axios from 'axios';
+
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
