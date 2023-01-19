ca07b76a65d68be4f7243888b93ae407f34bb81f
jmbvdev <jmvb.dev@gmail.com>
2023-01-19 13:12:32 -0400

    headers axios

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,8 @@ import store from './redux/store/index.js';
 import axios from 'axios';
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, PATCH, DELETE';
 
 
 
